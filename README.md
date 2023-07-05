7479f19c5ba4d2ef9c50b174b0ba8dad0cbfeb56
Tinker Monkey <1573324+tinkermonkey@users.noreply.github.com>
2023-07-05 19:27:41 -0400

    Display working via python

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # yardy_bot_spi_display
+
+## Setup
+- [Helpful link #1](https://learn.adafruit.com/adafruit-mini-pitft-135x240-color-tft-add-on-for-raspberry-pi/python-setup)
+- [Helpful link #2](https://learn.adafruit.com/adafruit-mini-pitft-135x240-color-tft-add-on-for-raspberry-pi/1-3-240x240-kernel-module-install)
+```
+sudo pip3 install --upgrade adafruit-python-shell click
+git clone https://github.com/adafruit/Raspberry-Pi-Installer-Scripts.git
+cd Raspberry-Pi-Installer-Scripts
+sudo python3 adafruit-pitft.py --display=st7789_240x240 --rotation=0 --install-type=console
+pip3 install adafruit-circuitpython-rgb-display
+pip3 install --upgrade --force-reinstall spidev
+sudo apt-get install fonts-dejavu
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo apt-get install python3-dev python3-rpi.gpio
+```
+
+- Set the console output to the display
+-- Add the following line to the end of `/boot/firmware/config.txt`:
+    ```
+    dtoverlay=spi1-3cs
+    ```
---
+++ sandbox/blank.py
@@ -0,0 +1,66 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+This demo will draw a few rectangles onto the screen along with some text
+on top of that.
+
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+
+Author(s): Melissa LeBlanc-Williams for Adafruit Industries
+"""
+
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_rgb_display import st7789  # pylint: disable=unused-import
+
+# First define some constants to allow easy resizing of shapes.
+BORDER = 0
+FONTSIZE = 24
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# pylint: disable=line-too-long
+# Create the display:
+disp = st7789.ST7789(
+    spi,
+    height=240,
+    y_offset=80,
+    rotation=180,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+# pylint: enable=line-too-long
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp.rotation % 180 == 90:
+    height = disp.width  # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width  # we swap height/width to rotate it to landscape!
+    height = disp.height
+
+image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a green filled box as the background
+draw.rectangle((0, 0, width, height), fill=(0, 0, 0))
+disp.image(image)
+
